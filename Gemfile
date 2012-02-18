88bd135775095f1eb4e44f42418bc1b7d5f25c15
Josh Adams <josh@isotope11.com>
2012-02-17 19:37:24 -0600

    first commit

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source :rubygems
+
+gem "rack", "1.3.5"
+gem "rack-contrib", :git => "https://github.com/rack/rack-contrib.git", :require => "rack/contrib"
+gem "grape", :git => "http://github.com/intridea/grape.git", :branch => "frontier"
---
+++ api/api.rb
@@ -0,0 +1,11 @@
+module Acme
+  class API < Grape::API
+    version 'v1', :using => :header, :vendor => 'acme', :format => :json    
+    resource :system do
+      desc "Returns pong."
+      get :ping do
+        { "pong" => true }
+      end
+    end
+  end
+end
